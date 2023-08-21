20dcf3e7cba7ba369cadcf0cfc07ff4f4c2df602
QTruonggg <quangtruong102000@gmail.com>
2023-08-21 15:20:37 +0700

    new

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Route } from 'react-router-dom';
+import './App.css';
+import SignUp from './components/Register/SignUp';
+
+
+function App() {
+  return (
+    <>
+      <SignUp/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/api/root.api.js
@@ -0,0 +1,1 @@
+export const API_URL = "http://14.225.7.221:8080";
